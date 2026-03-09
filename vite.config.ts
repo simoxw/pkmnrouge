39c9517c11b94d2247d15b330039a5ca0b5323f0
simoxw <enomisrasalgul@gmail.com>
2026-03-09 14:55:38 +0100

    fix pagina bianca

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     // SOSTITUISCI 'nome-del-tuo-repo' con il nome esatto della cartella su GitHub
-    base: '/pkmnrouge/', 
+    base: './', 
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
